d2386299092d55c2bfe7f29a7d3f77e1248aac85
liviutomo <ioan.liviu.tomoiaga>
2024-04-28 20:46:40 +0000

    set ec2 link for mlflow tracking uri
    Set AWS MLflow tracker

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import mlflow
 experiment_name = "ElasticNet"
 entry_point = "Training"
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri("http://ec2-13-60-64-97.eu-north-1.compute.amazonaws.com:5000/")
 
 mlflow.projects.run(
     uri=".",
