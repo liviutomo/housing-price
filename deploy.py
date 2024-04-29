7f3f479199a02afc01bb9bef2eec327f6fa96d3f
liviutomo <ioan.liviu.tomoiaga@gmail.com>
2024-04-29 12:19:20 +0300

    adapt config for deployment on sagemaker

--- deploy.py
+++ deploy.py
@@ -1,14 +1,14 @@
 import mlflow.sagemaker
 from mlflow.deployments import get_deploy_client
 
-endpoint_name="prod-endpoint"
+endpoint_name="prod:endpoint"
 model_uri="s3://mlflow-project-artifacts/4/d2ad59e0241c4f6f9212ff7e22ca780a/artifacts/XGBRegressor"
 
 # Define your configuration parameters as a dictionary
 config = {
-    "execution_role_arn": "arn:aws:iam::816680701120:role/house-price-role",
-    "bucket_name": "mlflow-project-artifacts",
-    "image_url": "816680701120.dkr.ecr.us-east-1.amazonaws.com/xgb:2.9.1",
+    "execution_role_arn": "arn:aws:iam::891377058700:role/mlops-project-housing-price-role",
+    "bucket_name": "mlops-mlflow-project-artifacts",
+    "image_url": "8891377058700.dkr.ecr.us-east-1.amazonaws.com/elastic-the-best:2.12.1",
     "region_name": "us-east-1",
     "archive": False,
     "instance_type": "ml.m5.xlarge",
